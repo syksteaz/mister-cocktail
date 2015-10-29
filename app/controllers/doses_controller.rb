4ebca9cf0a1a06272d2a3ee5b53b5e091564630d
edouard winia <edouardwinia@gmail.com>
2015-10-29 23:35:38 +0100

    last stuff done

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -6,16 +6,21 @@ class DosesController < ApplicationController
   end
 
   def create
-    @dose = @cocktail.doses.create(dose_params)
-    redirect_to cocktail_path(@cocktail)
+    @dose = @cocktail.doses.new(dose_params)
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
     # @ingredient = @cocktail.ingredients.create(ingredient_params
       # Pas necessaire de créer un @ingredient, on le récupère des dose params
     # STOP ICI : comment créer ma @dose avec dose_params & ingredient_params ?
   end
 
-  def delete
-    @dose = @cocktail.doses.delete(dose_params)
-    redirect_to cocktail_path(@cocktail)
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to :back
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
 
   resources :cocktails, only: [:index, :show, :new, :create] do
-    resources :doses, only: [:new, :create, :destroy]
+    resources :doses, only: [:new, :create]
   end
+
+  resources :doses, only: [:destroy]
 
   root 'cocktails#index'
 end
